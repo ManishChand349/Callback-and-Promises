ecf9b3c7515008048a88e8a8dc60d1f428875ffa
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-30 12:54:26 +0530

    Add files via upload

---
+++ 3. Age in Days/script.js
@@ -0,0 +1,16 @@
+function ageInDays(person) {
+  const fullName = `${person.firstName} ${person.lastName}`;
+  const ageInDays = person.age * 365;
+
+  return function() {
+    console.log(`The person's full name is ${fullName} and their age in days is ${ageInDays}.`);
+  }
+}
+const person = {
+  firstName: 'Manish',
+  lastName: 'Chand',
+  age: 21
+};
+
+const logAgeInDays = ageInDays(person);
+logAgeInDays(); 
